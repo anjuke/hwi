72dd8216976ea74404747f6bf619f3c9d49e1ba6
王强 <qiangwang@anjuke.com>
2013-01-07 10:42:31 +0800

    add btn class to <a> elements

--- src/main/webapp/assets/js/custom.js
+++ src/main/webapp/assets/js/custom.js
@@ -3,20 +3,26 @@ $(function(){
 	/* back to top */
     $(window).scroll(function () {
         if ($(this).scrollTop() > 400) {
-            $('#back-top').fadeIn();
+            $('#back-top').fadeIn()
         } else {
-            $('#back-top').fadeOut();
+            $('#back-top').fadeOut()
         }
     });
 
     $('#back-top').click(function () {
         $('body,html').animate({
             scrollTop: 0
-        }, 800);
-        return false;
+        }, 800)
+        return false
     });
 
     /* code syntax highlight */
-    hljs.initHighlightingOnLoad();
+    hljs.initHighlightingOnLoad()
+    
+
+    /* short query tooltip */
+    $('.short-query').tooltip({title:function(){
+    	return $(this).siblings('.long-query').text()
+    }})
     
 });
