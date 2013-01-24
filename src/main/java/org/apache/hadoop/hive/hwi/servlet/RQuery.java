4f2d0ce6a4c69e46308ae872c98cc0292b00fa25
孙志文 <zhiwensun@anjuke.com>
2013-01-24 10:48:39 +0800

    add LICENSE

--- src/main/java/org/apache/hadoop/hive/hwi/servlet/RQuery.java
+++ src/main/java/org/apache/hadoop/hive/hwi/servlet/RQuery.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) [2013] [Anjuke Inc]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.hive.hwi.servlet;
 
 import java.io.BufferedReader;
@@ -72,10 +87,10 @@ public class RQuery extends RBase {
             }
         }
         
-        l4j.info("------ list begin --------");
-        l4j.info("query classloader: " + query.getClass().getClassLoader());
-        l4j.info("MQuery classloader: " + MQuery.class.getClassLoader());
-        l4j.info("------ list   end --------");
+        l4j.debug("------ list begin --------");
+        l4j.debug("query classloader: " + query.getClass().getClassLoader());
+        l4j.debug("MQuery classloader: " + MQuery.class.getClassLoader());
+        l4j.debug("------ list   end --------");
 
         pagination = qs.paginate(query, map, page, pageSize);
 
